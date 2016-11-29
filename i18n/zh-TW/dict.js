fdbd25d1af601872d0ac266163951dc3c3983963
Baozi Wu <baojjeu@gmail.com>
2016-11-30 01:40:07 +0800

    Add dict for zh-TW

---
+++ i18n/zh-TW/dict.js
@@ -0,0 +1,45 @@
+module.exports = {
+    experience_type: {
+        "internship": "（實習）"
+    },
+    skill_type: {
+        "frontend": "Web 前端",
+        "backend": "Web 後端",
+        "miscellaneous": "其他"
+    },
+    education_type: {
+        "undergraduate": "（本科）"
+    },
+    link_type: {
+        "zhihu": "知乎",
+        "doubanbook": "豆瓣讀書",
+        "repo": "原始碼",
+        "pdf": "PDF 下載"
+    },
+    section_names: {
+        "education": {
+            "ch": "教育經歷",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "語言水準",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "專案與工作經驗",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "技能",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "你正在使用 IE8 或更老的古董瀏覽器，還是快升级或著換瀏覽器吧~",
+    dict: {
+        source_code: "原始碼",
+        demo: "Demo"
+    },
+    project_type: {
+        "personal": "個人專案"
+    }
+};
